97a5442de141fd2842a228d211aae786b7a71721
gicoder26 <gmantilla2696@gmail.com>
2026-05-07 17:46:26 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'astara-growth-radar-v2';
+const CACHE = 'astara-growth-radar-v3';
 const ASSETS = ['./','./index.html','./data.json','./manifest.json','./assets/astara-icon.svg','./assets/icon-180.png','./assets/icon-192.png','./assets/icon-512.png'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
